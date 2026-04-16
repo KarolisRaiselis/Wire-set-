053db33fe89ffa6f3a0f700b0211a320a0067ddb
KarolisRaiselis <karraiselis@gmail.com>
2026-04-16 10:56:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+import tkinter as tk
+from tkinter import filedialog
+from processing import process_file
 
+def select_file():
+    file_path = filedialog.askopenfilename(
+        title="Pasirink Excel failą",
+        filetypes=[("Excel files", "*.xlsx")]
+    )
+    
+    if file_path:
+        print("Pasirinktas failas:", file_path)
+        process_file(file_path)
+
+def main():
+    root = tk.Tk()
+    root.title("Wire-set")
+
+    root.geometry("300x150")
+
+    btn = tk.Button(root, text="Pasirink Excel failą", command=select_file)
+    btn.pack(expand=True)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
