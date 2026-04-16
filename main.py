1563093f8eb3fac7a701a6fc7f28e80671e6f18c
KarolisRaiselis <karraiselis@gmail.com>
2026-04-16 12:27:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,16 +24,15 @@ if "rows" not in st.session_state:
 
 with st.form("wire_form"):
     wires = []
-
- # 🔹 Viršutinė eilė (su apjungimu)
+# 🔹 Viršutinė eilė (su apjungimu)
 top = st.columns([2, 2, 1, 1, 1, 1, 1])
 
 with top[0]:
     st.markdown("<div style='text-align:center'><b>Komponentas 1</b></div>", unsafe_allow_html=True)
-    
+
 with top[1]:
     st.markdown("<div style='text-align:center'><b>Komponentas 2</b></div>", unsafe_allow_html=True)
-    
+
 top[2].markdown("**Laido pav.**")
 top[3].markdown("**Ilgis (mm)**")
 top[4].markdown("**Spalva**")
