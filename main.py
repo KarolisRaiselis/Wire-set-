3feee2585d59c66bdbce2d1937814c54805ea561
KarolisRaiselis <karraiselis@gmail.com>
2026-04-16 12:15:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import streamlit as st
 st.markdown("""
     <style>
     div[data-testid="stTextInput"] {
-        margin-bottom: -20px;
+        margin-bottom: -00px;
     }
     div[data-testid="stNumberInput"] {
-        margin-bottom: -10px;
+        margin-bottom: -30px;
     }
     </style>
 """, unsafe_allow_html=True)
